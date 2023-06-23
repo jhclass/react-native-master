6bf7bb8ae7ae6668f91f6eaa8fe4ed15ddc2a07c
Jinhyoung Lee <jhcode0@naver.com>
2023-06-23 11:16:47 +0900

    start

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import AppLoading from "expo-app-loading";
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => {
+    setLoading(false);
+  };
+  const preload = () => {
+    const fotToLoad = [Ionicons.font];
+
+    return Promise.all();
+  };
+
+  if (loading) {
+    return (
+      <AppLoading
+        onError={console.warn}
+        onFinish={onFinish}
+        startAsync={preload}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello everyone!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
