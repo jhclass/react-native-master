09e16a1afc17985adaa9981d94ded4681eb1efb4
Jinhyoung Lee <jhcode0@naver.com>
2023-06-26 19:38:23 +0900

    navigation

--- screens/Login.js
+++ screens/Login.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Text, View } from "react-native";
-const Login = () => {
+import { TouchableOpacity } from "react-native";
+const Login = ({ navigation }) => {
   return (
     <View>
       <Text>Login</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("createAccount")}>
+        <View>
+          <Text>Go to CreateAccount</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { Text, View } from "react-native";
-const Welcome = () => {
+import { Text, View, TouchableOpacity } from "react-native";
+const Welcome = ({ navigation }) => {
+  //console.log(props);
   return (
     <View>
-      <Text>Welcome</Text>
+      <Text>Welcome!</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+        <View>
+          <Text>Go to CreateAccount</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+        <View>
+          <Text>Login</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
