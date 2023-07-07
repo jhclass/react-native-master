c6a3aa3b45f306a1a8e470da3b6c5c7bfd354242
Jinhyoung Lee <jhcode0@naver.com>
2023-07-07 12:32:59 +0900

    Set react-native-web

---
+++ App.web.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function App() {
+  return (
+    <View>
+      <Text> 드디어 완성 </Text>
+    </View>
+  );
+}
+
+export default App;
