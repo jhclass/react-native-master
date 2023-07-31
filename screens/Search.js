6f3555c776b0930def5638a66e3ef942ae706027
Jinhyoung Lee <jhcode0@naver.com>
2023-07-31 18:42:12 +0900

    'Search' component -2

--- screens/Search.js
+++ screens/Search.js
@@ -1,5 +1,11 @@
-import React, { useEffect } from "react";
-import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  useWindowDimensions,
+  TouchableOpacity,
+} from "react-native";
 import { logoutFunc } from "../apollo";
 import styled from "styled-components";
 import DismissKeyboard from "../components/DismissKeyboard";
@@ -7,6 +13,8 @@ import { useForm } from "react-hook-form";
 import { gql, useLazyQuery } from "@apollo/client";
 import { ActivityIndicator } from "react-native";
 import { colors } from "../colors";
+import { FlatList } from "react-native-gesture-handler";
+
 const SEARCH_PHOTOS = gql`
   query ($keyword: String!) {
     searchPhotos(keyword: $keyword) {
@@ -19,6 +27,11 @@ const Input = styled.TextInput`
   /* width: 100px;
   height: 40px;
   background-color: #fff; */
+  background-color: rgba(255, 255, 255, 0.3);
+  width: ${(props) => props.width - 20};
+  padding: 5px;
+  border-radius: 5px;
+  color: #fff;
 `;
 const SearchingContainer = styled.View`
   flex: 1;
@@ -30,10 +43,13 @@ const Searching = styled.Text`
   margin-top: 10px;
 `;
 const Search = ({ navigation }) => {
+  const { width } = useWindowDimensions();
+  const numColumns = 3;
   const { register, setValue, watch, handleSubmit } = useForm();
+  const { refreshing, setRefreshing } = useState(false);
   //바로 실행되지 않음 (lazyQuery)
   //사용자가 글작성시 바로바로 나타나게 하려면 useQuery() 사용해야지
-  const [searchPhotoLazy, { loading, data, called }] = useLazyQuery(
+  const [searchPhotoLazy, { loading, data, called }, refetch] = useLazyQuery(
     SEARCH_PHOTOS
     //
     // {
@@ -42,6 +58,11 @@ const Search = ({ navigation }) => {
     //   },
     // }
   );
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
   //console.log(watch());
   const onSearchSubmit = ({ keyword }) => {
     //console.log("aaaaa");
@@ -56,10 +77,10 @@ const Search = ({ navigation }) => {
   console.log(data, "검색된 내용");
   const searchBox = () => {
     return (
-      <TextInput
-        style={{ backgroundColor: "#fff" }}
-        placeholderTextColor="black"
-        placeholder="Write search photo"
+      <Input
+        width={width}
+        placeholderTextColor="#fff"
+        placeholder="검색어를 입력하세요."
         autoCapitalize="none"
         returnKeyLabel="Search"
         returnKeyType="search"
@@ -77,6 +98,22 @@ const Search = ({ navigation }) => {
     register("keyword");
   }, [navigation]);
   //console.log(watch());
+  const renderSearchPhotos = ({ item: photos }) => {
+    //console.log(photos.file, "파일uri");
+    return (
+      <TouchableOpacity style={{ padding: 2 }}>
+        <Image
+          source={{ uri: photos.file }}
+          style={{
+            width: width / numColumns - 4,
+            height: width / numColumns - 4,
+
+            resizeMode: "cover",
+          }}
+        />
+      </TouchableOpacity>
+    );
+  };
   return (
     <DismissKeyboard>
       <View style={{ flex: 1, backgroundColor: "#000" }}>
@@ -92,11 +129,20 @@ const Search = ({ navigation }) => {
           </SearchingContainer>
         ) : null}
         {data?.searchPhotos !== undefined &&
-          data?.searchPhotos?.length === 0 && (
-            <SearchingContainer>
-              <Searching>아무것도 찾지 못하였습니다.</Searching>
-            </SearchingContainer>
-          )}
+        data?.searchPhotos?.length === 0 ? (
+          <SearchingContainer>
+            <Searching>아무것도 찾지 못하였습니다.</Searching>
+          </SearchingContainer>
+        ) : (
+          <FlatList
+            data={data?.searchPhotos}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            keyExtractor={(item, index) => String(item + index)}
+            renderItem={renderSearchPhotos}
+            numColumns={numColumns}
+          />
+        )}
       </View>
     </DismissKeyboard>
   );
